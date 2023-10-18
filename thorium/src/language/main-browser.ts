059e09f88babeda7071698dff37754e54a578c4f
Hugo LICHTENBERGER <76788589+LICHTENBERGERHugo@users.noreply.github.com>
2023-10-18 14:33:00 +0200

    feat: structure for dsl

---
+++ thorium/src/language/main-browser.ts
@@ -0,0 +1,14 @@
+import { EmptyFileSystem, startLanguageServer } from 'langium';
+import { BrowserMessageReader, BrowserMessageWriter, createConnection } from 'vscode-languageserver/browser.js';
+import { createThoriumServices } from './thorium-module.js';
+
+declare const self: DedicatedWorkerGlobalScope;
+
+const messageReader = new BrowserMessageReader(self);
+const messageWriter = new BrowserMessageWriter(self);
+
+const connection = createConnection(messageReader, messageWriter);
+
+const { shared } = createThoriumServices({ connection, ...EmptyFileSystem });
+
+startLanguageServer(shared);
