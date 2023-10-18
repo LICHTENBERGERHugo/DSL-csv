7e35512cd6069884b70ba46326d07f626ec10eea
Hugo LICHTENBERGER <76788589+LICHTENBERGERHugo@users.noreply.github.com>
2023-10-18 15:16:50 +0200

    feat: first valid grammar

--- thorium/src/language/thorium-validator.ts
+++ thorium/src/language/thorium-validator.ts
@@ -1,31 +1,35 @@
-import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { ThoriumAstType, Person } from './generated/ast.js';
-import type { ThoriumServices } from './thorium-module.js';
+import type { ValidationChecks } from "langium";
+import type { ThoriumAstType } from "./generated/ast.js";
+import type { ThoriumServices } from "./thorium-module.js";
 
 /**
  * Register custom validation checks.
  */
 export function registerValidationChecks(services: ThoriumServices) {
-    const registry = services.validation.ValidationRegistry;
-    const validator = services.validation.ThoriumValidator;
-    const checks: ValidationChecks<ThoriumAstType> = {
-        Person: validator.checkPersonStartsWithCapital
-    };
-    registry.register(checks, validator);
+  const registry = services.validation.ValidationRegistry;
+  const validator = services.validation.ThoriumValidator;
+  const checks: ValidationChecks<ThoriumAstType> = {
+    // Person: validator.checkPersonStartsWithCapital
+  };
+  registry.register(checks, validator);
 }
 
 /**
  * Implementation of custom validations.
  */
 export class ThoriumValidator {
-
-    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-        if (person.name) {
-            const firstChar = person.name.substring(0, 1);
-            if (firstChar.toUpperCase() !== firstChar) {
-                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
-            }
-        }
-    }
-
+  //   checkPersonStartsWithCapital(
+  //     // person: Person,
+  //     accept: ValidationAcceptor
+  //   ): void {
+  //     if (person.name) {
+  //       const firstChar = person.name.substring(0, 1);
+  //       if (firstChar.toUpperCase() !== firstChar) {
+  //         accept("warning", "Person name should start with a capital.", {
+  //           node: person,
+  //           property: "name",
+  //         });
+  //       }
+  //     }
+  //   }
 }
