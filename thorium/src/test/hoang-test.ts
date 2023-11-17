7a65860648d41f58bc13dbdd045835284e9dec2e
le-hoang.nguyen <lehoangnguyen510@gmail.com>
2023-11-17 11:21:37 +0100

    feats: add test

--- thorium/src/test/hoang-test.ts
+++ thorium/src/test/hoang-test.ts
@@ -1,15 +1,33 @@
-import { test, describe } from "node:test";
-//import { generatePython } from "../cli/generator.js";
+import { test, describe, expect } from "vitest";
+import { generatePython } from "../cli/generator.js";
+import { AstNode, EmptyFileSystem, LangiumDocument } from 'langium';
+import { parseDocument } from 'langium/test';
+import { createThoriumServices } from "../language/thorium-module.js";
+import { Model } from "../language/generated/ast.js";
+
+const services = createThoriumServices(EmptyFileSystem).Thorium; 
 
 describe('test hoang function', () => {
     test('print', async () => {
-        // const csv = await assertModelNoErrors(`
-        //     let csv = CSVFile("data.csv")
-        //     let table = Table(csv)
+        const model = await assertModelNoErrors(`
+            let csv = CSVFile("data.csv")
+            let table = Table(csv)
             
-        //     table.print()
-        // `)
-        // const file = generatePython(csv, "test", undefined)
-        
+            table.print()
+        `)
+        const test = generatePython(model, "data.csv", undefined);
+        expect(test).toBe(`import pandas as pd
+        csv= "data.csv"
+        table = pd.read_csv(csv)`
+        );
     })
 })
+
+async function assertModelNoErrors(modelText: string) : Promise<Model> {
+    var doc : LangiumDocument<AstNode> = await parseDocument(services, modelText)
+    const db = services.shared.workspace.DocumentBuilder
+    await db.build([doc], {validation: true});
+    const model = (doc.parseResult.value as Model);
+    expect(model.$document?.diagnostics?.length).toBe(0);
+    return model;    
+}
