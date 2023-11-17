d14b56a66393a905ae800ec88d217462bffa2ac3
Hugo LAMOUREUX <hugo.lamoureux18@gmail.com>
2023-11-17 10:06:51 +0100

    feat : compiler for add and delete

--- thorium/src/cli/generator.ts
+++ thorium/src/cli/generator.ts
@@ -1,7 +1,12 @@
 import {
+  DeleteParams,
   isAdd,
-  isComputation,
   isCSVFile,
+  isDelete,
+  isDeleteParamArrayInt,
+  isDeleteParamArrayString,
+  isDeleteParamInt,
+  isDeleteParamString,
   isTable,
   type Model,
 } from "../language/generated/ast.js";
@@ -20,7 +25,6 @@ export function generateJavaScript(
 
   const fileNode = new CompositeGeneratorNode();
   fileNode.append('"use strict";', NL, NL);
-  // model.greetings.forEach(greeting => fileNode.append(`console.log('Hello, ${greeting.person.ref?.name}!');`, NL));
 
   if (!fs.existsSync(data.destination)) {
     fs.mkdirSync(data.destination, { recursive: true });
@@ -51,9 +55,54 @@ export function generatePython(
   });
   model.functions.forEach((f) => {
     if (isAdd(f.ftype)) {
-      fileNode.append(`${f.table.name}.append(${f.ftype.parameters.row})`, NL);
-      if (isComputation(f.ftype)) {
-        fileNode.append(`${f.table}.shape[0]`);
+      if (f.ftype.parameters.row) {
+        fileNode.append(`values = "${f.ftype.parameters.row!.text}"`, NL);
+        fileNode.append(`new_row = pd.Series(values.split(","))`, NL);
+
+        fileNode.append(
+          `${f.table.name} = ${f.table.name}.append(new_row, ignore_index=True)`,
+          NL
+        );
+      } else {
+        fileNode.append(
+          `new_values = [${f.ftype.parameters
+            .rows!.rows.map((row) => '"' + row.text + '"')
+            .join(",")}]`,
+          NL
+        );
+        fileNode.append(`for row in new_values:`, NL);
+        fileNode.append(`\tvalues = row.split(',')`, NL);
+        fileNode.append(`\tnew_row = pd.Series(values)`, NL);
+        fileNode.append(
+          `\t${f.table.name} = ${f.table.name}.append(new_row, ignore_index=True)`,
+          NL
+        );
+      }
+    }
+    if (isDelete(f.ftype)) {
+      let params: DeleteParams = f.ftype.parameters;
+      if (isDeleteParamInt(params)) {
+        fileNode.append(
+          `${f.table.name} = ${f.table.name}.drop(${params.row})`,
+          NL
+        );
+      } else if (isDeleteParamString(params)) {
+        fileNode.append(
+          `${f.table.name} = ${f.table.name}.drop("${params.col}", axis=1)`,
+          NL
+        );
+      } else if (isDeleteParamArrayInt(params)) {
+        fileNode.append(
+          `${f.table.name} = ${f.table.name}.drop([${params.rows}])`,
+          NL
+        );
+      } else if (isDeleteParamArrayString(params)) {
+        fileNode.append(
+          `${f.table.name} = ${f.table.name}.drop([${params.cols
+            .map((e: any) => '"' + e + '"')
+            .join(",")}], axis=1)`,
+          NL
+        );
       }
     }
   });
