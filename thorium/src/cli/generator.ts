5e3f7228741848e6189b740d4a5069935b54e69d
Hugo LAMOUREUX <hugo.lamoureux18@gmail.com>
2023-11-17 11:05:56 +0100

    fix : parenthèses

--- thorium/src/cli/generator.ts
+++ thorium/src/cli/generator.ts
@@ -119,13 +119,15 @@ export function generatePython(
       if (isModify(f.ftype)) {
         if (isCSVRow(f.ftype.parameters.value)) {
           // Modify value of a whole row
-          const values = f.ftype.parameters.value.text.split(",").map((value) => {
-            const num = Number(value);
-            if (!isNaN(num)) {
-              return num;
-            }
-            return `"${value}"`;
-          });
+          const values = f.ftype.parameters.value.text
+            .split(",")
+            .map((value) => {
+              const num = Number(value);
+              if (!isNaN(num)) {
+                return num;
+              }
+              return `"${value}"`;
+            });
           const row = `${f.table.name}.loc[${f.ftype.parameters.rowID}] = [${values}]`;
           fileNode.append(row, NL);
         } else if (typeof f.ftype.parameters.colID === "string") {
@@ -137,6 +139,7 @@ export function generatePython(
           const cell = `${f.table.name}.at[${f.ftype.parameters.rowID}, ${f.ftype.parameters.colID}] = ${f.ftype.parameters.value}`;
           fileNode.append(cell, NL);
         }
+      }
       if (isAdd(f.ftype)) {
         if (f.ftype.parameters.row) {
           fileNode.append(`values = "${f.ftype.parameters.row!.text}"`, NL);
@@ -241,13 +244,15 @@ export function generatePython(
 }
 
 // take a file_path as input and return the result of the execution along with execution time and memory usage
-export async function execPython(file_path : string): Promise<string> {
+export async function execPython(file_path: string): Promise<string> {
   const PYTHON_INTERPRETER = "python3.8"; // python version to use
 
   try {
-      const result = await new Promise<string>((resolve, reject) => {
-        const startTime = process.hrtime(); // Start measuring execution time
-        exec(PYTHON_INTERPRETER + " " + file_path + "'", (error, stdout, stderr) => {
+    const result = await new Promise<string>((resolve, reject) => {
+      const startTime = process.hrtime(); // Start measuring execution time
+      exec(
+        PYTHON_INTERPRETER + " " + file_path + "'",
+        (error, stdout, stderr) => {
           const endTime = process.hrtime(startTime); // Calculate execution time
           const executionTime = `${endTime[0]}s ${endTime[1] / 1000000}ms`; // Format execution time
 
@@ -260,16 +265,19 @@ export async function execPython(file_path : string): Promise<string> {
           } else {
             console.log(`stdout: ${stdout}`);
             console.log(`Execution Time: ${executionTime}`);
-            console.log(`Memory Consumption: ${process.memoryUsage().heapUsed / 1024 / 1024} MB`);
+            console.log(
+              `Memory Consumption: ${
+                process.memoryUsage().heapUsed / 1024 / 1024
+              } MB`
+            );
             resolve(stdout as string);
           }
-        });
-      });
-      return result.trim();
+        }
+      );
+    });
+    return result.trim();
   } catch (error) {
-      console.log(error);
-      return `Error: ${error}`;
+    console.log(error);
+    return `Error: ${error}`;
   }
 }
-
-
