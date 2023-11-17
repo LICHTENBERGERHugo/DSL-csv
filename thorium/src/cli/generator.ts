a5c3e32acb8dfc38b85f0e2afaa787d8cf094646
Hugo LICHTENBERGER <76788589+LICHTENBERGERHugo@users.noreply.github.com>
2023-11-17 10:38:03 +0100

    fix: add declaration case

--- thorium/src/cli/generator.ts
+++ thorium/src/cli/generator.ts
@@ -14,8 +14,8 @@ import {
   isFilter,
   isFilterParams,
   isConditionArray,
-} 
-from "../language/generated/ast.js";
+  isComputation,
+} from "../language/generated/ast.js";
 import * as fs from "node:fs";
 import { CompositeGeneratorNode, NL, toString } from "langium";
 import * as path from "node:path";
@@ -34,9 +34,10 @@ export function generateJavaScript(
 
   model.functions.forEach((f) => {
     if (isThoriumFunction(f)) {
-      if (isPrint(f.ftype) ) {
+      if (isPrint(f.ftype)) {
         //const df = f.table.name;
-        fileNode.append(`
+        fileNode.append(
+          `
           fs.createReadStream("${filePath}")
             .pipe(csv())
             .on("data", (row) => {
@@ -44,7 +45,9 @@ export function generateJavaScript(
           })
           .on("end", () => {
           console.log("CSV reading completed.");
-        });`, NL);
+        });`,
+          NL
+        );
       }
       if (isFilter(f.ftype)) {
         let str = "";
@@ -55,20 +58,19 @@ export function generateJavaScript(
             if (isConditionArray(conditions)) {
               const condition1 = conditions.con1;
               const other = conditions.other;
-              
+
               str += `${f.table.name}['${condition1.rowname}'] ${condition1.argument} ${condition1.value}`;
               let others = "";
               if (other != null) {
-                
                 //others = `(${f.table.name}['${other.rowname}'] ${other.argument} ${other.value})`;
               }
               str = "(" + str + ") & " + others;
             }
-          }else if(condition != null){
+          } else if (condition != null) {
             str += `${f.table.name}['${condition.rowname}'] ${condition.argument} ${condition.value}`;
           }
         }
-        fileNode.append(`${f.table.name}[${str}]`,NL);
+        fileNode.append(`${f.table.name}[${str}]`, NL);
       }
     }
   });
@@ -90,16 +92,23 @@ export function generatePython(
   fileNode.append("import pandas as pd", NL);
   model.declarations.forEach((declaration) => {
     if (isTable(declaration)) {
-      fileNode.append(
-        `${declaration.name} = pd.read_csv(${declaration.file?.name})`, NL
-      );
+      if (declaration.file?.name) {
+        fileNode.append(
+          `${declaration.name} = pd.read_csv(${declaration.file?.name})`,
+          NL
+        );
+      } else {
+        fileNode.append(
+          `${declaration.name} = pd.read_csv("${declaration.file?.filepath}")`,
+          NL
+        );
+      }
     }
     if (isCSVFile(declaration)) {
       fileNode.append(`${declaration.name}= "${declaration.filepath}"`, NL);
     }
   });
   model.functions.forEach((f) => {
-
     if (isAdd(f.ftype)) {
       if (f.ftype.parameters.row) {
         fileNode.append(`values = "${f.ftype.parameters.row!.text}"`, NL);
@@ -126,10 +135,10 @@ export function generatePython(
       }
     }
     if (isThoriumFunction(f)) {
-      if (isPrint(f.ftype) ) {
+      if (isPrint(f.ftype)) {
         const df = f.table.name;
         fileNode.append(`print(${df}.to_string())`, NL);
-      } 
+      }
       if (isComputation(f.ftype)) {
         if (f.ftype.agg == "COUNT") {
           fileNode.append(`${f.table.name}.shape[0]`, NL);
@@ -138,32 +147,32 @@ export function generatePython(
           fileNode.append(`${f.table.name}["${f.ftype.cname}"].sum()`, NL);
         }
       }
-          if (isDelete(f.ftype)) {
-      let params: DeleteParams = f.ftype.parameters;
-      if (isDeleteParamInt(params)) {
-        fileNode.append(
-          `${f.table.name} = ${f.table.name}.drop(${params.row})`,
-          NL
-        );
-      } else if (isDeleteParamString(params)) {
-        fileNode.append(
-          `${f.table.name} = ${f.table.name}.drop("${params.col}", axis=1)`,
-          NL
-        );
-      } else if (isDeleteParamArrayInt(params)) {
-        fileNode.append(
-          `${f.table.name} = ${f.table.name}.drop([${params.rows}])`,
-          NL
-        );
-      } else if (isDeleteParamArrayString(params)) {
-        fileNode.append(
-          `${f.table.name} = ${f.table.name}.drop([${params.cols
-            .map((e: any) => '"' + e + '"')
-            .join(",")}], axis=1)`,
-          NL
-        );
+      if (isDelete(f.ftype)) {
+        let params: DeleteParams = f.ftype.parameters;
+        if (isDeleteParamInt(params)) {
+          fileNode.append(
+            `${f.table.name} = ${f.table.name}.drop(${params.row})`,
+            NL
+          );
+        } else if (isDeleteParamString(params)) {
+          fileNode.append(
+            `${f.table.name} = ${f.table.name}.drop("${params.col}", axis=1)`,
+            NL
+          );
+        } else if (isDeleteParamArrayInt(params)) {
+          fileNode.append(
+            `${f.table.name} = ${f.table.name}.drop([${params.rows}])`,
+            NL
+          );
+        } else if (isDeleteParamArrayString(params)) {
+          fileNode.append(
+            `${f.table.name} = ${f.table.name}.drop([${params.cols
+              .map((e: any) => '"' + e + '"')
+              .join(",")}], axis=1)`,
+            NL
+          );
+        }
       }
-          }
       if (isFilter(f.ftype)) {
         let str = "";
         if (isFilterParams(f.ftype.parameters)) {
@@ -173,23 +182,25 @@ export function generatePython(
             if (isConditionArray(conditions)) {
               const condition1 = conditions.con1;
               const other = conditions.other;
-              
+
               str += `${f.table.name}['${condition1.rowname}'] ${condition1.argument} ${condition1.value}`;
               let others = "";
               if (other != null) {
                 const len = other.length;
                 for (let i = 0; i < len - 1; i++) {
                   others += `(${f.table.name}['${other[i].rowname}'] ${other[i].argument} ${other[i].value}) & `;
-                }  
-                others += `(${f.table.name}['${other[len-1].rowname}'] ${other[len-1].argument} ${other[len-1].value})`;
+                }
+                others += `(${f.table.name}['${other[len - 1].rowname}'] ${
+                  other[len - 1].argument
+                } ${other[len - 1].value})`;
               }
               str = "(" + str + ") & " + others;
             }
-          }else if(condition != null){
+          } else if (condition != null) {
             str += `${f.table.name}['${condition.rowname}'] ${condition.argument} ${condition.value}`;
           }
         }
-        fileNode.append(`${f.table.name}[${str}]`,NL);
+        fileNode.append(`${f.table.name}[${str}]`, NL);
       }
     }
   });
