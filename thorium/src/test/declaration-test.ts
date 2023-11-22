c3513d96cdc9d9374eb7baf695348da5bf81605f
Hugo LICHTENBERGER <76788589+LICHTENBERGERHugo@users.noreply.github.com>
2023-11-22 16:50:18 +0100

    fix: deleting await

--- thorium/src/test/declaration-test.ts
+++ thorium/src/test/declaration-test.ts
@@ -16,7 +16,7 @@ describe("Test basic declarations", () => {
         `);
     const file = generatePython(model, "test", undefined);
 
-    await fs.readFile("./" + file, "utf8", function (err, data) {
+    fs.readFile("./" + file, "utf8", function (err, data) {
       if (err) {
         console.log(err);
       } else {
