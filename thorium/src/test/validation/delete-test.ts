b1712ea86301e26432e7da748c3bee92f0636f62
Hugo LICHTENBERGER <76788589+LICHTENBERGERHugo@users.noreply.github.com>
2023-11-27 09:42:40 +0100

    feat: validation tests for R and python (without modify and filter)

--- thorium/src/test/validation/delete-test.ts
+++ thorium/src/test/validation/delete-test.ts
@@ -2,31 +2,60 @@ import { describe, expect, test } from "vitest";
 import { Model } from "../../language/generated/ast.js";
 import { parseDocument } from "langium/test";
 import { AstNode, EmptyFileSystem, LangiumDocument } from "langium";
-import { generatePython } from "../../cli/generator.js";
+import { generatePython, generateR } from "../../cli/generator.js";
 import { createThoriumServices } from "../../language/thorium-module.js";
 import * as fs from "node:fs";
 
 const services = createThoriumServices(EmptyFileSystem).Thorium;
 
-describe("Test delete", () => {
-  test("correct formation", async () => {
-    const model = await assertModelNoErrors(`
-    let table = Table(CSVFile("data.csv"))
+const th3Code = `
+let csv = CSVFile("data.csv")
+let table = Table(csv)
 
-    table.delete(2)
-    table.delete("age")
-    table.delete([1,2,3]) 
-    table.delete(["name","departement"])
-    `);
-    const file = generatePython(model, "test", undefined);
+table.delete(2)
+table.delete("age")
+table.delete([1,2,3])
+table.delete(["name","departement"])
+`;
+
+const expectedPython = `import pandas as pd
+csv = "data.csv"
+table = pd.read_csv(csv)
+table = table.drop(2)
+table = table.drop("age", axis=1)
+table = table.drop([1,2,3])
+table = table.drop(["name","departement"], axis=1)
+`;
+const expectedR = `csv <- "data.csv"
+table <- read.csv(csv, stringsAsFactors = FALSE)
+table <- table[-2,]
+table <- table[,!names(table) %in% c("age")]
+table <- table[-c(1,2,3),]
+table <- table[,!names(table) %in% c("name","departement")]
+`;
+
+describe("Test delete", () => {
+  test("correct python code", async () => {
+    const model = await assertModelNoErrors(th3Code);
+    const file = generatePython(model, "delete", undefined);
 
     fs.readFile("./" + file, "utf8", function (err, data) {
       if (err) {
         console.log(err);
       } else {
-        expect(data).toEqual(
-          `import pandas as pd\r\ntable = pd.read_csv("data.csv")\r\ntable = table.drop(2)\r\ntable = table.drop("age", axis=1)\r\ntable = table.drop([1,2,3])\r\ntable = table.drop(["name","departement"], axis=1)\r\n`
-        );
+        expect(data.replace(/\r\n/g, "\n")).toEqual(expectedPython);
+      }
+    });
+  });
+  test("correct R code", async () => {
+    const model = await assertModelNoErrors(th3Code);
+    const file = generateR(model, "delete", undefined);
+
+    fs.readFile("./" + file, "utf8", function (err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        expect(data.trim().replace(/\r\n/g, "\n")).toEqual(expectedR.trim());
       }
     });
   });
--- thorium/src/test/validation/project-test.ts
+++ thorium/src/test/validation/project-test.ts
@@ -7,34 +7,39 @@ import { createThoriumServices } from "../../language/thorium-module.js";
 import * as fs from "node:fs";
 
 const services = createThoriumServices(EmptyFileSystem).Thorium;
-
-const expectedPython = `import pandas as pd
-csv = "data.csv"
-table = pd.read_csv(csv)
-`;
 const th3Code = `
-let csv = CSVFile("data.csv")
-let table = Table(csv)
-`;
-const expectedR = `csv <- "data.csv"
-table <- read.csv(csv, stringsAsFactors = FALSE)`;
+let csv1 = CSVFile("data.csv")
+let table = Table(CSVFile("data.csv"))
 
-describe("Test basic declarations", () => {
+table.project("name")
+table.project(["name","age"])
+`;
+const expectedPython = `import pandas as pd
+csv1 = "data.csv"
+table = pd.read_csv("data.csv")
+table["name"]
+table[["name","age"]]
+`;
+const expectedR = `csv1 <- "data.csv"
+table <- read.csv("data.csv")
+table <- table[,c("name")]
+table <- table[,c("name","age")]`;
+describe("Test validate project", () => {
   test("correct python code", async () => {
     const model = await assertModelNoErrors(th3Code);
-    const file = generatePython(model, "declaration", undefined);
+    const file = generatePython(model, "project", undefined);
 
     fs.readFile("./" + file, "utf8", function (err, data) {
       if (err) {
         console.log(err);
       } else {
-        expect(data.replace(/\r\n/g, "\n")).toBe(expectedPython);
+        expect(data.replace(/\r\n/g, "\n")).toEqual(expectedPython);
       }
     });
   });
   test("correct R code", async () => {
     const model = await assertModelNoErrors(th3Code);
-    const file = generateR(model, "declaration", undefined);
+    const file = generateR(model, "project", undefined);
 
     fs.readFile("./" + file, "utf8", function (err, data) {
       if (err) {
